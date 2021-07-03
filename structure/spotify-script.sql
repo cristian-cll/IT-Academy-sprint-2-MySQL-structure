f566cc183bdafc39550d378dd9cb895255934ee3
cristian-cll <78435266+cristian-cll@users.noreply.github.com>
2021-07-04 00:15:24 +0200

    Fix errors on Mysql structure/script

--- structure/spotify-script.sql
+++ structure/spotify-script.sql
@@ -1,72 +1,39 @@
 DROP DATABASE IF EXISTS spotify;
 CREATE DATABASE spotify CHARACTER SET utf8mb4;
 
+
 -- -----------------------------------------------------
 -- Table `spotify`.`users`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `spotify`.`users` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `email` VARCHAR(45) NULL,
-  `password` VARCHAR(45) NULL,
-  `user_name` VARCHAR(45) NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  `user_name` VARCHAR(45) NOT NULL,
   `birth_date` DATETIME NULL,
   `gender` ENUM("Female", "Male") NULL,
   `country` VARCHAR(45) NULL,
   `post_code` VARCHAR(10) NULL,
-  `role` ENUM("Free", "Premium") NULL,
+  `role` ENUM("Free", "Premium") NOT NULL DEFAULT 'Free',
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `spotify`.`credit_cards`
+-- Table `spotify`.`paymentMethod`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `spotify`.`credit_cards` (
+CREATE TABLE IF NOT EXISTS `spotify`.`paymentMethod` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `number` BIGINT NULL,
-  `month` INT NULL,
-  `year` INT NULL,
-  `cvv` INT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `spotify`.`paypals`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `spotify`.`paypals` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `user_name` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `spotify`.`subscriptions`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `spotify`.`subscriptions` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `start_date` DATETIME NULL,
-  `renewal_date` DATETIME NULL,
-  `pay_method` ENUM("CC", "PayPal") NULL,
-  `credit_cards_id` INT NULL,
-  `paypals_id` INT NULL,
   `users_id` INT NOT NULL,
+  `type` ENUM("CC", "PayPal") NOT NULL,
+  `cc_number` VARCHAR(45) NULL,
+  `cc_month` INT NULL,
+  `cc_year` INT NULL,
+  `cc_cvv` INT NULL,
+  `pp_user_name` VARCHAR(45) NULL,
+  INDEX `fk_paymentMethod_users1_idx` (`users_id` ASC) VISIBLE,
   PRIMARY KEY (`id`),
-  INDEX `fk_subscriptions_credit_cards1_idx` (`credit_cards_id` ASC) VISIBLE,
-  INDEX `fk_subscriptions_paypals1_idx` (`paypals_id` ASC) VISIBLE,
-  INDEX `fk_subscriptions_users1_idx` (`users_id` ASC) VISIBLE,
-  CONSTRAINT `fk_subscriptions_credit_cards1`
-    FOREIGN KEY (`credit_cards_id`)
-    REFERENCES `spotify`.`credit_cards` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_subscriptions_paypals1`
-    FOREIGN KEY (`paypals_id`)
-    REFERENCES `spotify`.`paypals` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_subscriptions_users1`
+  CONSTRAINT `fk_paymentMethod_users1`
     FOREIGN KEY (`users_id`)
     REFERENCES `spotify`.`users` (`id`)
     ON DELETE NO ACTION
@@ -79,14 +46,41 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `spotify`.`payments` (
   `id` INT NOT NULL AUTO_INCREMENT,
+  `order_num` VARCHAR(45) NULL,
   `date` DATETIME NULL,
-  `price` VARCHAR(45) NULL,
-  `users_id` INT NOT NULL,
+  `price` FLOAT NULL,
+  `paymentMethod_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_Payments_users1_idx` (`users_id` ASC) VISIBLE,
-  CONSTRAINT `fk_Payments_users1`
+  UNIQUE INDEX `order_num_UNIQUE` (`order_num` ASC) VISIBLE,
+  INDEX `fk_payments_paymentMethod1_idx` (`paymentMethod_id` ASC) VISIBLE,
+  CONSTRAINT `fk_payments_paymentMethod1`
+    FOREIGN KEY (`paymentMethod_id`)
+    REFERENCES `spotify`.`paymentMethod` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `spotify`.`subscriptions`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `spotify`.`subscriptions` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `start_date` DATETIME NULL,
+  `renewal_date` DATETIME NULL,
+  `users_id` INT NOT NULL,
+  `payments_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_subscriptions_users1_idx` (`users_id` ASC) VISIBLE,
+  INDEX `fk_subscriptions_payments1_idx` (`payments_id` ASC) VISIBLE,
+  CONSTRAINT `fk_subscriptions_users1`
     FOREIGN KEY (`users_id`)
     REFERENCES `spotify`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_subscriptions_payments1`
+    FOREIGN KEY (`payments_id`)
+    REFERENCES `spotify`.`payments` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -151,7 +145,7 @@ CREATE TABLE IF NOT EXISTS `spotify`.`songs` (
   `name` VARCHAR(45) NULL,
   `listenings` INT NULL,
   `albums_id` INT NOT NULL,
-  PRIMARY KEY (`id`, `albums_id`),
+  PRIMARY KEY (`id`),
   INDEX `fk_songs_albums1_idx` (`albums_id` ASC) VISIBLE,
   CONSTRAINT `fk_songs_albums1`
     FOREIGN KEY (`albums_id`)
@@ -211,18 +205,25 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `spotify`.`playlists_has_songs` (
   `playlists_id` INT NOT NULL,
   `songs_id` INT NOT NULL,
-  `songs_albums_id` INT NOT NULL,
-  PRIMARY KEY (`playlists_id`, `songs_id`, `songs_albums_id`),
-  INDEX `fk_playlists_has_songs_songs1_idx` (`songs_id` ASC, `songs_albums_id` ASC) VISIBLE,
+  `users_id` INT NOT NULL,
+  `added_at` DATETIME NOT NULL,
+  PRIMARY KEY (`playlists_id`, `songs_id`, `users_id`),
+  INDEX `fk_playlists_has_songs_songs1_idx` (`songs_id` ASC) VISIBLE,
   INDEX `fk_playlists_has_songs_playlists1_idx` (`playlists_id` ASC) VISIBLE,
+  INDEX `fk_playlists_has_songs_users1_idx` (`users_id` ASC) VISIBLE,
   CONSTRAINT `fk_playlists_has_songs_playlists1`
     FOREIGN KEY (`playlists_id`)
     REFERENCES `spotify`.`playlists` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_playlists_has_songs_songs1`
-    FOREIGN KEY (`songs_id` , `songs_albums_id`)
-    REFERENCES `spotify`.`songs` (`id` , `albums_id`)
+    FOREIGN KEY (`songs_id`)
+    REFERENCES `spotify`.`songs` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_playlists_has_songs_users1`
+    FOREIGN KEY (`users_id`)
+    REFERENCES `spotify`.`users` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -234,9 +235,8 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `spotify`.`users_has_favourite_songs` (
   `users_id` INT NOT NULL,
   `songs_id` INT NOT NULL,
-  `songs_albums_id` INT NOT NULL,
-  PRIMARY KEY (`users_id`, `songs_id`, `songs_albums_id`),
-  INDEX `fk_users_has_songs_songs1_idx` (`songs_id` ASC, `songs_albums_id` ASC) VISIBLE,
+  PRIMARY KEY (`users_id`, `songs_id`),
+  INDEX `fk_users_has_songs_songs1_idx` (`songs_id` ASC) VISIBLE,
   INDEX `fk_users_has_songs_users1_idx` (`users_id` ASC) VISIBLE,
   CONSTRAINT `fk_users_has_songs_users1`
     FOREIGN KEY (`users_id`)
@@ -244,41 +244,34 @@ CREATE TABLE IF NOT EXISTS `spotify`.`users_has_favourite_songs` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_users_has_songs_songs1`
-    FOREIGN KEY (`songs_id` , `songs_albums_id`)
-    REFERENCES `spotify`.`songs` (`id` , `albums_id`)
+    FOREIGN KEY (`songs_id`)
+    REFERENCES `spotify`.`songs` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `spotify`.`users_add_songs_playlist`
+-- Table `spotify`.`users_shared_playlists`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `spotify`.`users_add_songs_playlist` (
+CREATE TABLE IF NOT EXISTS `spotify`.`users_shared_playlists` (
   `users_id` INT NOT NULL,
-  `songs_id` INT NOT NULL,
-  `songs_albums_id` INT NOT NULL,
   `playlists_id` INT NOT NULL,
-  PRIMARY KEY (`users_id`, `songs_id`, `songs_albums_id`, `playlists_id`),
-  INDEX `fk_users_has_songs_songs2_idx` (`songs_id` ASC, `songs_albums_id` ASC) VISIBLE,
-  INDEX `fk_users_has_songs_users2_idx` (`users_id` ASC) VISIBLE,
-  INDEX `fk_users_add_songs_playlist_playlists1_idx` (`playlists_id` ASC) VISIBLE,
-  CONSTRAINT `fk_users_has_songs_users2`
+  PRIMARY KEY (`users_id`, `playlists_id`),
+  INDEX `fk_users_has_playlists_playlists1_idx` (`playlists_id` ASC) VISIBLE,
+  INDEX `fk_users_has_playlists_users1_idx` (`users_id` ASC) VISIBLE,
+  CONSTRAINT `fk_users_has_playlists_users1`
     FOREIGN KEY (`users_id`)
     REFERENCES `spotify`.`users` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_users_has_songs_songs2`
-    FOREIGN KEY (`songs_id` , `songs_albums_id`)
-    REFERENCES `spotify`.`songs` (`id` , `albums_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_users_add_songs_playlist_playlists1`
+  CONSTRAINT `fk_users_has_playlists_playlists1`
     FOREIGN KEY (`playlists_id`)
     REFERENCES `spotify`.`playlists` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
+
 
 
 -- -----------------------------------------------------
@@ -295,21 +288,21 @@ COMMIT;
 
 
 -- -----------------------------------------------------
--- Data for table `spotify`.`credit_cards`
+-- Data for table `spotify`.`paymentMethod`
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `spotify`;
-INSERT INTO `spotify`.`credit_cards` (`id`, `number`, `month`, `year`, `cvv`) VALUES (DEFAULT, 05999874848484, 06, 2025, 000);
+INSERT INTO `spotify`.`paymentMethod` (`id`, `users_id`, `type`, `cc_number`, `cc_month`, `cc_year`, `cc_cvv`, `pp_user_name`) VALUES (DEFAULT, 1, 'CC', '089895904984', 09, 2025, 000, NULL);
 
 COMMIT;
 
 
 -- -----------------------------------------------------
--- Data for table `spotify`.`paypals`
+-- Data for table `spotify`.`payments`
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `spotify`;
-INSERT INTO `spotify`.`paypals` (`id`, `user_name`) VALUES (DEFAULT, 'juan_01');
+INSERT INTO `spotify`.`payments` (`id`, `order_num`, `date`, `price`, `paymentMethod_id`) VALUES (DEFAULT, '2021-00000001', '2021-06-25', 20, 1);
 
 COMMIT;
 
@@ -319,8 +312,7 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `spotify`;
-INSERT INTO `spotify`.`subscriptions` (`id`, `start_date`, `renewal_date`, `pay_method`, `credit_cards_id`, `paypals_id`, `users_id`) VALUES (DEFAULT, '2021-06-25', '2022-06-25', 'CC', 1, NULL, 2);
-INSERT INTO `spotify`.`subscriptions` (`id`, `start_date`, `renewal_date`, `pay_method`, `credit_cards_id`, `paypals_id`, `users_id`) VALUES (DEFAULT, '2021-05-21', '2022-05-21', 'PayPal', NULL, 1, 3);
+INSERT INTO `spotify`.`subscriptions` (`id`, `start_date`, `renewal_date`, `users_id`, `payments_id`) VALUES (DEFAULT, '2021-06-25', '2022-06-25', 2, 1);
 
 COMMIT;
 
@@ -381,6 +373,18 @@ COMMIT;
 
 
 -- -----------------------------------------------------
+-- Data for table `spotify`.`users_follow_artists`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `spotify`;
+INSERT INTO `spotify`.`users_follow_artists` (`users_id`, `artists_id`) VALUES (1, 1);
+INSERT INTO `spotify`.`users_follow_artists` (`users_id`, `artists_id`) VALUES (1, 2);
+INSERT INTO `spotify`.`users_follow_artists` (`users_id`, `artists_id`) VALUES (1, 3);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
 -- Data for table `spotify`.`artists_relationed`
 -- -----------------------------------------------------
 START TRANSACTION;
@@ -395,8 +399,10 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `spotify`;
-INSERT INTO `spotify`.`playlists_has_songs` (`playlists_id`, `songs_id`, `songs_albums_id`) VALUES (1, 1, 3);
-INSERT INTO `spotify`.`playlists_has_songs` (`playlists_id`, `songs_id`, `songs_albums_id`) VALUES (1, 2, 8);
+INSERT INTO `spotify`.`playlists_has_songs` (`playlists_id`, `songs_id`, `users_id`, `added_at`) VALUES (1, 1, 1, '2021-06-29');
+INSERT INTO `spotify`.`playlists_has_songs` (`playlists_id`, `songs_id`, `users_id`, `added_at`) VALUES (1, 2, 1, '2021-06-29');
+INSERT INTO `spotify`.`playlists_has_songs` (`playlists_id`, `songs_id`, `users_id`, `added_at`) VALUES (1, 3, 2, '2021-06-28');
+INSERT INTO `spotify`.`playlists_has_songs` (`playlists_id`, `songs_id`, `users_id`, `added_at`) VALUES (1, 4, 3, '2021-07-01');
 
 COMMIT;
 
@@ -406,45 +412,38 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `spotify`;
-INSERT INTO `spotify`.`users_has_favourite_songs` (`users_id`, `songs_id`, `songs_albums_id`) VALUES (1, 1, 3);
-INSERT INTO `spotify`.`users_has_favourite_songs` (`users_id`, `songs_id`, `songs_albums_id`) VALUES (1, 2, 8);
-INSERT INTO `spotify`.`users_has_favourite_songs` (`users_id`, `songs_id`, `songs_albums_id`) VALUES (1, 4, 4);
+INSERT INTO `spotify`.`users_has_favourite_songs` (`users_id`, `songs_id`) VALUES (1, 1);
+INSERT INTO `spotify`.`users_has_favourite_songs` (`users_id`, `songs_id`) VALUES (1, 2);
+INSERT INTO `spotify`.`users_has_favourite_songs` (`users_id`, `songs_id`) VALUES (1, 4);
 
 COMMIT;
 
 
 -- -----------------------------------------------------
--- Data for table `spotify`.`users_add_songs_playlist`
+-- Data for table `spotify`.`users_shared_playlists`
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `spotify`;
-INSERT INTO `spotify`.`users_add_songs_playlist` (`users_id`, `songs_id`, `songs_albums_id`, `playlists_id`) VALUES (1, 1, 3, 1);
+INSERT INTO `spotify`.`users_shared_playlists` (`users_id`, `playlists_id`) VALUES (1, 1);
+INSERT INTO `spotify`.`users_shared_playlists` (`users_id`, `playlists_id`) VALUES (1, 2);
+INSERT INTO `spotify`.`users_shared_playlists` (`users_id`, `playlists_id`) VALUES (1, 3);
 
 COMMIT;
 
 
 
+-- Proves
 
--- Consultes de prova
-
--- Les playlists que tinguin un status "Active" d'un usuari
-
-USE spotify;
-SELECT p.title Playlist_name, u.user_name
-FROM playlists p
-INNER JOIN users u
-ON p.users_id = u.id
-WHERE u.user_name = "jose_01" AND p.status = "Active";
-
--- Les cançons favorites d'un usuari
+-- Mostra el nom de l'usuari i les cançons agregades a una playlist que pertanyi a un altre usuari
+-- L'usuari jose_01 ha creat una playlist "Rock", quins altres usuaris han afegir cançons?
 
 USE spotify;
-SELECT u.user_name, s.name Fav_Song, a.title Album
-FROM songs s
-INNER JOIN users_has_favourite_songs uhfv
-ON uhfv.songs_id = s.id
-INNER JOIN users u
-ON u.id = uhfv.users_id
-INNER JOIN albums a
-ON a.id = s.albums_id
-WHERE u.user_name = "jose_01";
+SELECT u.user_name, s.name Song, p.title Playlist_title
+FROM users u
+INNER JOIN playlists_has_songs phs
+ON u.id = phs.users_id
+INNER JOIN playlists p
+ON phs.playlists_id = p.id
+INNER JOIN songs s
+ON s.id = phs.songs_id
+WHERE p.users_id != u.id
--- structure/youtube-script.sql
+++ structure/youtube-script.sql
@@ -1,5 +1,6 @@
 DROP DATABASE IF EXISTS youtube;
 CREATE DATABASE youtube CHARACTER SET utf8mb4;
+
 -- -----------------------------------------------------
 -- Table `youtube`.`channels`
 -- -----------------------------------------------------
@@ -17,8 +18,8 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `youtube`.`users` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `email` VARCHAR(45) NULL,
-  `username` VARCHAR(45) NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `username` VARCHAR(45) NOT NULL,
   `birth_date` DATE NULL,
   `gender` VARCHAR(45) NULL,
   `country` VARCHAR(45) NULL,
@@ -45,8 +46,10 @@ CREATE TABLE IF NOT EXISTS `youtube`.`videos` (
   `file_name` VARCHAR(20) NULL,
   `length` INT NULL,
   `thumbnail` BLOB NULL,
-  `views` INT NULL,
+  `views` INT NOT NULL DEFAULT 0,
   `status` ENUM("Public", "Hidden", "Private") NULL,
+  `likes` INT NOT NULL DEFAULT 0,
+  `dislikes` INT NOT NULL DEFAULT 0,
   `user` INT NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_videos_users1_idx` (`user` ASC) VISIBLE,
@@ -120,7 +123,7 @@ CREATE TABLE IF NOT EXISTS `youtube`.`playlists` (
   `playlist_name` VARCHAR(45) NULL,
   `create_on` DATE NULL,
   `users_id` INT NOT NULL,
-  `status` ENUM("Private", "Public") NULL,
+  `status` ENUM("Private", "Public") NOT NULL DEFAULT 'Public',
   PRIMARY KEY (`id`),
   INDEX `fk_playlists_users1_idx` (`users_id` ASC) VISIBLE,
   CONSTRAINT `fk_playlists_users1`
@@ -223,3 +226,161 @@ CREATE TABLE IF NOT EXISTS `youtube`.`playlists_has_videos` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
+
+
+
+-- -----------------------------------------------------
+-- Data for table `youtube`.`channels`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `youtube`;
+INSERT INTO `youtube`.`channels` (`id`, `channel_name`, `description`, `created_on`) VALUES (DEFAULT, 'Rincón de Jose', 'Entertainment', NULL);
+INSERT INTO `youtube`.`channels` (`id`, `channel_name`, `description`, `created_on`) VALUES (DEFAULT, 'Rincón de Laura', 'Fun', NULL);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `youtube`.`users`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `youtube`;
+INSERT INTO `youtube`.`users` (`id`, `email`, `username`, `birth_date`, `gender`, `country`, `post_code`, `channels_id`) VALUES (DEFAULT, 'jose@gmail.com', 'jose_01', '1990-05-25', 'Male', 'Spain', '08008', 1);
+INSERT INTO `youtube`.`users` (`id`, `email`, `username`, `birth_date`, `gender`, `country`, `post_code`, `channels_id`) VALUES (DEFAULT, 'laura@gmail.com', 'laura_01', '1992-04-28', 'Female', 'Spain', '09897', 2);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `youtube`.`videos`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `youtube`;
+INSERT INTO `youtube`.`videos` (`id`, `title`, `description`, `size`, `file_name`, `length`, `thumbnail`, `views`, `status`, `likes`, `dislikes`, `user`) VALUES (DEFAULT, 'Monkey jumping', 'Monkey jumping at forest ', 4, 'monkey_jumping', 3, NULL, DEFAULT, NULL, DEFAULT, DEFAULT, 1);
+INSERT INTO `youtube`.`videos` (`id`, `title`, `description`, `size`, `file_name`, `length`, `thumbnail`, `views`, `status`, `likes`, `dislikes`, `user`) VALUES (DEFAULT, 'Cats smiling', 'Cats smiling and singing', 6, 'cats_smiling', 5, NULL, DEFAULT, NULL, DEFAULT, DEFAULT, 2);
+INSERT INTO `youtube`.`videos` (`id`, `title`, `description`, `size`, `file_name`, `length`, `thumbnail`, `views`, `status`, `likes`, `dislikes`, `user`) VALUES (DEFAULT, 'Persevernace on Mars', 'NASA\'s perseverance', 10, 'perseverance_on_mars', 8, NULL, DEFAULT, NULL, DEFAULT, DEFAULT, 1);
+INSERT INTO `youtube`.`videos` (`id`, `title`, `description`, `size`, `file_name`, `length`, `thumbnail`, `views`, `status`, `likes`, `dislikes`, `user`) VALUES (DEFAULT, 'Cats sleeping', 'Cats slepping on sofa', 2, 'cats_sleeping', 2, NULL, DEFAULT, NULL, DEFAULT, DEFAULT, 2);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `youtube`.`tags`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `youtube`;
+INSERT INTO `youtube`.`tags` (`id`, `tag_name`) VALUES (DEFAULT, 'animals');
+INSERT INTO `youtube`.`tags` (`id`, `tag_name`) VALUES (DEFAULT, 'cats');
+INSERT INTO `youtube`.`tags` (`id`, `tag_name`) VALUES (DEFAULT, 'space');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `youtube`.`videos_has_tags`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `youtube`;
+INSERT INTO `youtube`.`videos_has_tags` (`tags_id`, `videos_id`) VALUES (1, 1);
+INSERT INTO `youtube`.`videos_has_tags` (`tags_id`, `videos_id`) VALUES (2, 2);
+INSERT INTO `youtube`.`videos_has_tags` (`tags_id`, `videos_id`) VALUES (3, 3);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `youtube`.`channels_has_subscribers`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `youtube`;
+INSERT INTO `youtube`.`channels_has_subscribers` (`channels_id`, `users_id`) VALUES (1, 2);
+INSERT INTO `youtube`.`channels_has_subscribers` (`channels_id`, `users_id`) VALUES (2, 1);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `youtube`.`playlists`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `youtube`;
+INSERT INTO `youtube`.`playlists` (`id`, `playlist_name`, `create_on`, `users_id`, `status`) VALUES (DEFAULT, 'Animales', NULL, 1, 'Public');
+INSERT INTO `youtube`.`playlists` (`id`, `playlist_name`, `create_on`, `users_id`, `status`) VALUES (DEFAULT, 'Cats', NULL, 2, 'Public');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `youtube`.`comments`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `youtube`;
+INSERT INTO `youtube`.`comments` (`id`, `text`, `create_on`, `user`, `videos_id`) VALUES (DEFAULT, 'Wow! I like it', '2021-07-04', 2, 1);
+INSERT INTO `youtube`.`comments` (`id`, `text`, `create_on`, `user`, `videos_id`) VALUES (DEFAULT, 'hahahah they are pretty', '2021-07-01', 1, 2);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `youtube`.`users_likes_videos`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `youtube`;
+INSERT INTO `youtube`.`users_likes_videos` (`users_id`, `videos_id`, `is_like`) VALUES (1, 2, 1);
+INSERT INTO `youtube`.`users_likes_videos` (`users_id`, `videos_id`, `is_like`) VALUES (2, 1, 1);
+INSERT INTO `youtube`.`users_likes_videos` (`users_id`, `videos_id`, `is_like`) VALUES (2, 3, 0);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `youtube`.`users_likes_comments`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `youtube`;
+INSERT INTO `youtube`.`users_likes_comments` (`users_id`, `comments_id`, `is_like`, `date`) VALUES (1, 1, '1', NULL);
+INSERT INTO `youtube`.`users_likes_comments` (`users_id`, `comments_id`, `is_like`, `date`) VALUES (2, 2, '1', NULL);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `youtube`.`playlists_has_videos`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `youtube`;
+INSERT INTO `youtube`.`playlists_has_videos` (`videos_id`, `playlists_id`) VALUES (1, 1);
+INSERT INTO `youtube`.`playlists_has_videos` (`videos_id`, `playlists_id`) VALUES (2, 2);
+INSERT INTO `youtube`.`playlists_has_videos` (`videos_id`, `playlists_id`) VALUES (4, 2);
+
+COMMIT;
+
+
+
+-- Proves
+
+-- Mostra els videos de cada playlist i a qui pertany
+
+USE youtube;
+SELECT p.playlist_name, v.title video, u.username playlist_owner
+FROM playlists p
+INNER JOIN playlists_has_videos phv
+ON phv.playlists_id = p.id 
+INNER JOIN videos v
+ON phv.videos_id = v.id
+INNER JOIN users u
+ON u.id = v.user;
+
+
+
+-- Mostra el comentaris que existeixen, el video a qui estan assignats, el propietari del video i el text del comentari
+
+USE youtube;
+SELECT owner.username Owner_video, v.title Video, u.username Comment_from, c.text Comment
+FROM comments c
+INNER JOIN videos v
+ON c.videos_id = v.id
+INNER JOIN users u
+ON u.id = c.user
+INNER JOIN users owner
+ON owner.id = v.id;
